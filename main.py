3e0061793ee9ef2070ba0449a864ccc5d18441ce
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-06-16 15:42:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from pokemon import *
+from pokemon import * #es buena idea usar *? nos ahorra los prefijos pero tiene mucho costo de memoria o procesamiento? ####################
 
 def main():
-    #juego = juego_crear()
+    juego = crear_juego()
 
     # Ajustar el tamaño de la ventana
     gamelib.resize(ANCHO_VENTANA, ALTO_VENTANA)
@@ -10,9 +10,10 @@ def main():
     while gamelib.is_alive():
         # Todas las instrucciones que dibujen algo en la pantalla deben ir
         # entre `draw_begin()` y `draw_end()`:
-        gamelib.draw_begin()
-        menu_principal()
-        gamelib.draw_end()
+        #gamelib.draw_begin()
+        if juego == 1:
+            menu_principal()
+        #gamelib.draw_end() # CREO QUE EL DIBUJO VA EN CADA FUNCION. LO COMENTO PARA NO BORRARLO ##########################
 
         # Terminamos de dibujar la ventana, ahora procesamos los eventos (si el
         # usuario presionó una tecla o un botón del mouse, etc).
@@ -31,6 +32,9 @@ def main():
         if ev.type == gamelib.EventType.ButtonPress:
             # El usuario presionó un botón del mouse
             x, y = ev.x, ev.y # averiguamos la posición donde se hizo click
+            print (f'x {x}, y {y}')
             #juego = juego_actualizar(juego, x, y)
+            juego = pokemones_o_equipos(x, y)
 
 gamelib.init(main)
+
