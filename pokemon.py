9c3e668c618c272b07020ac43f1c708f29ae7171
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-06-20 04:31:13 -0300

    Update pokemon.py
    Agregado el botón de retroceder

--- pokemon.py
+++ pokemon.py
@@ -15,6 +15,7 @@ XY_CUADRITO = 100
 MRG_CUADRITOS_SUP = 90
 MRG_CUADRITOS_IZQ = 70
 ESP_ENTRE_CUADROS = 10
+BOTON_RETROCESO = 30
 
 def crear_juego():
     return 'menu principal'
@@ -31,6 +32,7 @@ def menu_principal():
     gamelib.draw_text('POKEMONES', MRG_HORZ_BOTONES, ALTO_VENTANA // 2 - ALTO_BOTONES, fill='black', size=25, anchor='nw') #Texto de botón izq.
     gamelib.draw_text('EQUIPOS', ANCHO_VENTANA // 2 +  ESP_ENTRE_BOTON, ALTO_VENTANA // 2 - ALTO_BOTONES, fill='black', size=25, anchor='nw') #Texto de botón der.
     gamelib.draw_end()
+    return 'menu principal'
 
 def cuadritos_pokemones(): 
     nro_pok_nombre = {0: VACIO, 1:'pikachu', 2:'bulbasur', 3:'charmander', 4:'hello', 5:'pikachu', 6:'bulbasur', 7:'charmander', 8:'hello', 9:'pikachu', 10:'bulbasur', 11:'charmander', 12:'hello', 13:'pikachu', 14:'bulbasur', 15:'charmander', 16:'hello', 17:'pikachu', 18:'bulbasur', 19:'charmander', 20:'hello', 21:'pikachu', 22:'bulbasur', 23:'charmander', 24:'hello', 25:'pikachu', 26:'bulbasur', 27:'charmander', 28:'hello', 29:'pikachu', 30:'bulbasur', 31:'pikachu', 32:'bulbasur', 33:'charmander', 34:'hello', 35:'pikachu', 36:'bulbasur', 37:'charmander', 38:'hello', 39:'pikachu', 40:'bulbasur', 41:'charmander', 42:'hello', }
@@ -77,9 +79,27 @@ def un_pokemon(nro_pokemon):
     gamelib.draw_text(velocidad, 4* ANCHO_VENTANA // 5, 6 * ALTO_VENTANA // 7, fill='black', size=30,)
     gamelib.draw_text(spee     , 4* ANCHO_VENTANA // 5, 7 * ALTO_VENTANA // 7, fill='black', size=30,) # se podrían ahorrar lineas con un for
     gamelib.draw_image(imagen, VACIO, ALTO_VENTANA // 4)
-
+    gamelib.draw_rectangle(BOTON_RETROCESO, BOTON_RETROCESO, BOTON_RETROCESO*2, BOTON_RETROCESO*2, fill = 'red')
     gamelib.draw_end()
     return 'Individual Pokemon'
+
+def un_equipo(nro_equipo):
+    """
+    nombre = 'Omega' #Acá implementación del nombre del pokemon
+    integrantes = {1:[lista de ataques], 2:[lista de ataques], 3:[lista de ataques]}
+    @ = nombre, lista de ataques
+    gamelib.draw_text(@1, 4* ANCHO_VENTANA // 5, 2 * ALTO_VENTANA // 7, fill='black', size=30,)
+    gamelib.draw_text(@2   , 4* ANCHO_VENTANA // 5, 3 * ALTO_VENTANA // 7, fill='black', size=30,)
+    gamelib.draw_text(@3, 4* ANCHO_VENTANA // 5, 4 * ALTO_VENTANA // 7, fill='black', size=30,)
+    gamelib.draw_text(@4, 4* ANCHO_VENTANA // 5, 5 * ALTO_VENTANA // 7, fill='black', size=30,) #acá iría el nombre de cada pokemon integrante del equipo y sus ataques elegidos. Sacar info del csv que creamos nosotros
+    gamelib.draw_text(@5, 4* ANCHO_VENTANA // 5, 6 * ALTO_VENTANA // 7, fill='black', size=30,)
+    gamelib.draw_text(@6, 4* ANCHO_VENTANA // 5, 7 * ALTO_VENTANA // 7, fill='black', size=30,)
+    """
+    gamelib.draw_begin()
+    gamelib.draw_rectangle(BOTON_RETROCESO, BOTON_RETROCESO, BOTON_RETROCESO*2, BOTON_RETROCESO*2, fill = 'red')
+    gamelib.draw_end()
+    imagen = 'imgs/1.gif'
+    return 'Individual Equipo'
 
 def navegacion(x, y, juego): #implementado con diccionarios
     selector = {}       
@@ -92,6 +112,7 @@ def navegacion(x, y, juego): #implementado con diccionarios
         gamelib.draw_rectangle(VACIO, VACIO, ANCHO_VENTANA, ALTO_VENTANA)
         gamelib.draw_text(selector['pokequipos'], ANCHO_VENTANA // 2, TITLE_Y, fill='black', size=30, anchor='s')
         cuadritos_pokemones()
+        gamelib.draw_rectangle(BOTON_RETROCESO, BOTON_RETROCESO, BOTON_RETROCESO*2, BOTON_RETROCESO*2, fill = 'red')
         gamelib.draw_end()
         return 'menu ' + selector['pokequipos']
 
@@ -101,24 +122,52 @@ def navegacion(x, y, juego): #implementado con diccionarios
         gamelib.draw_rectangle(VACIO, VACIO, ANCHO_VENTANA, ALTO_VENTANA)
         gamelib.draw_text(selector['equipos'], ANCHO_VENTANA // 2, TITLE_Y, fill='black', size=30, anchor='s')
         cuadritos_equipos()
+        gamelib.draw_rectangle(BOTON_RETROCESO, BOTON_RETROCESO, BOTON_RETROCESO*2, BOTON_RETROCESO*2, fill = 'red')
         gamelib.draw_end()
         return 'menu ' + selector['equipos']
-
+    """
     if juego == 'menu principal' and x > MRG_HORZ_BOTONES and x < ANCHO_VENTANA // 2 -  ESP_ENTRE_BOTON \
        and y > BTN_Y1 and y < BTN_Y2:
         return menu_pokemones()
-        
-    elif juego == 'menu principal' and x > ANCHO_VENTANA // 2 + ESP_ENTRE_BOTON and x < ANCHO_VENTANA - MRG_HORZ_BOTONES \
-       and y > BTN_Y1 and y < BTN_Y2: #podriamos poner todos los juego == menu principal en un mismo if
-        selector['pokemones'] = 'equipos'
-        return menu_equipos()
+    """     
+    if juego == 'menu principal':
+        if x > ANCHO_VENTANA // 2 + ESP_ENTRE_BOTON and x < ANCHO_VENTANA - MRG_HORZ_BOTONES \
+        and y > BTN_Y1 and y < BTN_Y2: #podriamos poner todos los juego == menu principal en un mismo if
+            selector['pokemones'] = 'equipos'
+            return menu_equipos()
+        elif x > MRG_HORZ_BOTONES and x < ANCHO_VENTANA // 2 -  ESP_ENTRE_BOTON \
+        and y > BTN_Y1 and y < BTN_Y2:
+            return menu_pokemones()
 
     elif juego == 'menu Pokemones':
+        if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
+        and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
+            return menu_principal()
         xcuadro = (x - MRG_CUADRITOS_IZQ) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         ycuadro = (y - MRG_CUADRITOS_SUP) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         #print ('cuadro: ', xcuadro, ycuadro)
         nro_pokemon = 1 #nro_pokemon = ycuadro * 7 + xcuadro + nro_pag_pok ####Implementar, elige el pokemon que el usuario clickeó
         return un_pokemon(nro_pokemon)
+
+    elif juego == 'menu Equipos':
+        if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
+        and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
+            return menu_principal()
+        xcuadro = (x - MRG_CUADRITOS_IZQ) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
+        ycuadro = (y - MRG_CUADRITOS_SUP) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
+        #print ('cuadro: ', xcuadro, ycuadro)
+        nro_equipo = 1 #nro_pokemon = ycuadro * 7 + xcuadro + nro_pag_pok ####Implementar, elige el pokemon que el usuario clickeó
+        return un_equipo(nro_equipo)
+
+    elif juego == 'Individual Pokemon':
+        if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
+        and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
+            return menu_pokemones()
+
+    elif juego == 'Individual Equipo':
+        if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
+        and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
+            return menu_equipos()
 
     return juego
 
