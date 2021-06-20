8cf3b7872705656056a08b3a816043a1fa207137
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-06-20 03:28:18 -0300

    Muestra de como quedaría con la gameboy.

---
+++ maingameboy.py
@@ -0,0 +1,30 @@
+from pokemongameboy import * 
+
+def main():
+    juego = crear_juego()
+
+    gamelib.resize(ANCHO_VENTANAGAMEBOY, ALTO_VENTANAGAMEBOY)
+
+    while gamelib.is_alive():
+
+        if juego == 'menu principal':
+            #print ('funca')
+            menu_principal()
+
+        ev = gamelib.wait()
+
+        if not ev:
+            break
+
+        if ev.type == gamelib.EventType.KeyPress and ev.key == 'Escape':
+            break
+
+        if ev.type == gamelib.EventType.ButtonPress:
+            x, y = ev.x, ev.y 
+            print (f'x {x}, y {y}') #debug
+            #juego = juego_actualizar(juego, x, y)
+            #print (juego)
+            juego = navegacion(x, y, juego) #le cambié el nombre a la funcion
+            print ('Juego', juego)
+
+gamelib.init(main)
