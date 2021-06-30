d3485e163cfba2cf425906e1d56cd1bcd180a44c
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-06-30 05:13:09 -0300

    Limpieza

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from pokemon import *
 
 def main():
     juego = crear_juego()
-
     gamelib.resize(ANCHO_VENTANA, ALTO_VENTANA)
 
     while gamelib.is_alive():
-
         if juego[0] == 'menu principal':
-            #print ('funca')
             menu_principal()
 
         ev = gamelib.wait()
@@ -21,10 +18,6 @@ def main():
 
         if ev.type == gamelib.EventType.ButtonPress:
             x, y = ev.x, ev.y 
-            #print (f'x {x}, y {y}') #debug
-            #juego = juego_actualizar(juego, x, y)
-            #print (juego)
-            juego = navegacion(x, y, juego) #le cambié el nombre a la funcion
-            #print ('Juego', juego)
+            juego = navegacion(x, y, juego)
 
 gamelib.init(main)
