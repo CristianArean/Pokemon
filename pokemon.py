5a16440739aaa6ab39c85dab24e9ae3dc424a336
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-06-30 03:06:55 -0300

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -18,15 +18,14 @@ MRG_CUADRITOS_IZQ = 70
 ESP_ENTRE_CUADROS = 10
 BOTON_RETROCESO = 30
 
-
-
 def crear_juego():
     menu_memorizado = 'menu principal'
-    with open('equipos.csv', 'w'):
-        pass
+    with open('equipos.csv', 'w') as archivo:
+        for _ in range (1):
+            archivo.writelines('equipo_nombre;integrante1;movimientos1;integrante2;movimientos2;integrante3;movimientos3;integrante4;movimientos4;integrante5;movimientos5;integrante6;movimientos6')
     return 'menu principal', 0, 0
 
-def lector(nros, nombre_archivo): #cambiar el nombre por "lector"
+def lector(nros, nombre_archivo): 
     """
     Entrega toda la info de las lineas que solicites (tomando la primera linea como cero, para ignorarla). Retorna un diccionario
     Puede usarse para el archivo de pokemones y de equipos, ya sea para menús o para mostrar la info de un pokemon o equipo en particular.
@@ -49,6 +48,15 @@ def lector(nros, nombre_archivo): #cambiar el nombre por "lector"
         lista_atributos = (lineas_solicitadas.desencolar()).split(';')
         rta[lista_atributos[0]] = lista_atributos
     return rta
+
+def lector_movimientos(nro, nombre_archivo):
+    contador = 1
+    with open(nombre_archivo) as archivo:
+        for linea in archivo:
+            leido = linea.readline()
+            contador += 1
+            if contador == nro:
+                return leido.split(';')
 
 def menu_principal(): 
     gamelib.draw_begin()
@@ -86,8 +94,6 @@ def menu_equipos(pag_pok, pag_equ):
 
 def cuadritos_pokemones(pag_pok, pag_equ): 
     nro_pok_nombre = lector([pag_pok*4*7 -10, pag_pok*4*7 +28], 'pokemons.csv') #ajustar numeros mágicos
-    print (nro_pok_nombre)
-    #print (nro_pok_nombre)
     for i in range (4): #arreglar numeros mágicos
         for j in range (7): 
             gamelib.draw_rectangle(MRG_CUADRITOS_IZQ + ESP_ENTRE_CUADROS * (j+1) + XY_CUADRITO * j, MRG_CUADRITOS_SUP + ESP_ENTRE_CUADROS * (i+1) + XY_CUADRITO * i, MRG_CUADRITOS_IZQ + ESP_ENTRE_CUADROS * (j+1) + XY_CUADRITO * j + XY_CUADRITO, MRG_CUADRITOS_SUP + ESP_ENTRE_CUADROS * (i+1) + XY_CUADRITO * i + XY_CUADRITO)
@@ -143,35 +149,43 @@ def un_equipo(nro_equipo, pag_pok, pag_equ):
     menu_memorizado = 'Individual Equipo', pag_pok, pag_equ
     return 'Individual Equipo', pag_pok, pag_equ
 
-def creador_equipo(juego):
+def creador_equipo():
     pokemones_elegidos = []
-    nombre_equipo = input("Ingrese el nombre del equipo")
-    while len(pokemones_elegidos) >= 5:
-        ataques_elegidos = 0
-        #proceso para agregar pokemones
+    nombre_equipo = simpledialog.askstring("nombre de equipo", "Elija un nombre para su equipo").upper()
+    desea_seguir_pokemones = "SI"
+    while len(pokemones_elegidos) >= 5 and desea_seguir_pokemones == "SI":
+        elegido = simpledialog.askstring("pokemones", "que pokemon desea elegir?")
+        while elegido is not elegido.isdigit():
+            elegido = simpledialog.askstring("pokemones", "Ingreso un carater no numerico. Que pokemon desea elegir?")
+        pokemones_elegidos.append(int(elegido)) #se agrega el pokemon
+        if len(pokemones_elegidos) != 0: #si la lista tiene algun elemento te pregunta si queres borrar
+            desea_eliminar_pokemones = simpledialog.askstring("pokemones", "Desea borrar algun pokemon?[SI/NO]").upper()
+            while desea_eliminar_pokemones not in["SI", "NO"]:
+                desea_eliminar_pokemones = simpledialog.askstring("pokemones", "Ingreso una respuesta incorrecta. Desea borrar algun pokemon?[SI/NO]")
+            if desea_eliminar_pokemones == "SI": #pregunta que pokemon queremos eliminar
+                pokemon_a_eliminar = simpledialog.askstring("pokemones", "Ingrese el numero de pokemon que desea borrar")
+                while not pokemon_a_eliminar is pokemon_a_eliminar.isdigit() and pokemon_a_eliminar not in pokemones_elegidos:
+                    pokemon_a_eliminar = simpledialog.askstring("pokemones", "El caracter es invalido o no posee ese pokemon. Ingrese el numero de pokemon que desea borrar")
+                pokemones_elegidos.remove(int(pokemon_a_eliminar))
+            elif desea_eliminar_pokemones == "NO":
+                continue #acá habia dudas
+        #te pregunta si queres agregar pokemones
         desea_seguir_pokemones = simpledialog.askstring("pokemones", "desea seguir agregando pokemones?[SI/NO]").upper()
-        while desea_seguir_pokemones not in["Si", "No"]:
+        while desea_seguir_pokemones not in["SI", "NO"]:
             desea_seguir_pokemones = simpledialog.askstring("pokemones", "No eligio una respusta valida. Desea seguir agregando pokemones[SI/NO]").upper()
-        if desea_seguir_pokemones == "SI":
-            pass
-        
-        elif desea_seguir_pokemones == "NO":
-            break
+    for monstruo in pokemones_elegidos:
+        info = lector_movimientos(monstruo, 'movimientos.csv')
+        lista_movimientos = info[1].split(',')
+    #agregar cómo se escribe al equipos
 
-        if len(pokemones_elegidos) == 5:
-            terminaste = 
-            while terminaste not in["SI", "NO"]:
-                terminaste = 
-            if terminaste ==  "NO":
-                nro_pokemon_seleccionado = simpledialog.askstring("pokemones", "Ingrese el numero de pokemon que desea borrar").upper()
-                while nro_pokemon_seleccionado not in pokemones_elegidos:
-                    nro_pokemon_seleccionado = simpledialog.askstring("pokemones", "Ese no es un pokemon que eligio. Ingrese el numero de pokemon que quiere borrar:").upper()
-                pokemones_elegidos.remove(nro_pokemon_seleccionado)
-
+def menu_creador(pag_pok, pag_equ):
+    #dibujar
+    #llamar a creador_equipo()
     pass
+
 def navegacion(x, y, juego):
 
-    print (juego)
+    #print (juego)
     if juego[0] == 'menu principal':
         p_pok = juego [1]
         p_equ = juego [2]
@@ -185,10 +199,10 @@ def navegacion(x, y, juego):
     elif juego[0] == 'menu Pokemones':
         pag_pok = juego[1]
         pag_equ = juego[2]
-        print (pag_pok)
         if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
         and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
             return menu_principal() #BOTON ROJO
+
         xcuadro = (x - MRG_CUADRITOS_IZQ) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         ycuadro = (y - MRG_CUADRITOS_SUP) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         #print ('cuadro: ', xcuadro, ycuadro)
@@ -207,11 +221,20 @@ def navegacion(x, y, juego):
         if x > BOTON_RETROCESO and x < BOTON_RETROCESO*2 \
         and y > BOTON_RETROCESO and y < BOTON_RETROCESO*2:
             return menu_principal() #BOTON ROJO
+            
         xcuadro = (x - MRG_CUADRITOS_IZQ) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         ycuadro = (y - MRG_CUADRITOS_SUP) // (XY_CUADRITO + ESP_ENTRE_CUADROS)
         #print ('cuadro: ', xcuadro, ycuadro)
-        nro_equipo = 1 
-        return un_equipo(nro_equipo, pag_pok, pag_equ)
+        nro_equipo = ycuadro * 7 + xcuadro + pag_equ*26
+        if (nro_equipo - pag_equ*26) == 0:
+            if pag_pok > 0: return menu_pokemones(pag_pok, pag_equ-1)
+            return menu_pokemones(pag_pok, pag_equ)
+        if (nro_equipo - pag_equ*26) == 27:
+            return menu_pokemones(pag_pok, pag_equ+1)
+        if nro_equipo == 1:
+            return menu_creador(pag_pok, pag_equ)
+        if nro_equipo > 1:
+            return un_equipo(nro_equipo, pag_pok, pag_equ) #equipos
 
     elif juego[0] == 'Individual Pokemon':
         pag_pok = juego[1]
@@ -228,4 +251,4 @@ def navegacion(x, y, juego):
             return menu_pokemones(pag_pok, pag_equ) #BOTON ROJO
 
     return 'menu principal', 0, 0 #####Esto arregla en parte el bug donde los clicks dejan de hacer efecto. Sucede que
-    # el estado de juego se vuelve none (en la linea 27 de main. Arreglar con menu_memorizado) MIENTRAS TANTO CADA VEZ QUE FALLA SIMPLEMENTE NOS LLEVA AL MENU PRINCIPAL, así no se rompe.
+    # el estado de juego se vuelve none (en la linea 27 de main. Arreglar con menu_memorizado) MIENTRAS TANTO CADA VEZ QUE FALLA SIMPLEMENTE NOS LLEVA AL MENU
