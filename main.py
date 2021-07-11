1f5595be60774de5e9c130ab0dc6368b8fbfc87f
Santiago <84828241+astrocinco@users.noreply.github.com>
2021-07-11 12:48:56 -0300

    Sin import gamelib

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import pokemon
-from pokemon import gamelib
 
 ANCHO_VENTANA = 900
 ALTO_VENTANA = 600
 
 def main():
     juego = pokemon.crear_juego()
-    gamelib.resize(ANCHO_VENTANA, ALTO_VENTANA)
+    pokemon.gamelib.resize(ANCHO_VENTANA, ALTO_VENTANA)
 
-    while gamelib.is_alive():
+    while pokemon.gamelib.is_alive():
         if juego[0] == 'menu principal':
             pokemon.menu_principal()
 
-        ev = gamelib.wait()
+        ev = pokemon.gamelib.wait()
 
         if not ev:
             break
 
-        if ev.type == gamelib.EventType.KeyPress and ev.key == 'Escape':
+        if ev.type == pokemon.gamelib.EventType.KeyPress and ev.key == 'Escape':
             break
 
-        if ev.type == gamelib.EventType.ButtonPress:
+        if ev.type == pokemon.gamelib.EventType.ButtonPress:
             x, y = ev.x, ev.y 
             juego = pokemon.navegacion(x, y, juego)
 
-gamelib.init(main)
+pokemon.gamelib.init(main)
